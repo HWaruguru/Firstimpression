13fd21039f2cf0e75a8348147880633b8f009b7a
HWaruguru <hannah.waruguru8@gmail.com>
2019-10-21 17:37:39 +0300

    First commit

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+class Config:
+    DEBUG = False
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+
+class ProdConfig(Config):
+    pass
+
+
+class TestConfig(Config):
+    pass 
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+config_options = {
+'development': DevConfig,
+'production': ProdConfig,
+'test': TestConfig
+}
