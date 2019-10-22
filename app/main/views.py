4ef4f70e427039d6f39211537ec7928e93fb0e6b
HWaruguru <hannah.waruguru8@gmail.com>
2019-10-22 23:39:13 +0300

    Add a signup view in views.py

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,7 @@
 from flask import render_template,request,redirect,url_for,abort
 from . import main
+from app.models import User
+from werkzeug.security import generate_password_hash
 
 
 @main.route('/')
@@ -8,13 +10,24 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return "jkbnjktrnobnorgtbr"
+    return render_template('index.html')
 
-
-@main.route('/signup')
+@main.route('/signup', methods=['GET', 'POST'])
 def signup():
     '''
     View root page function that returns the index page and its data
     '''
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email)
+        if user:
+            return render_template('signup.html', error=f'User with email {email} already exists')
+        new_user = User(username=username, email=email, password=generate_password_hash(password, method='sha256'))
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('main.login'))
     return render_template('signup.html')
 
