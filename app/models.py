8f2e439ff8400f58ef504470cb7ab5636efb0fa1
HWaruguru <hannah.waruguru8@gmail.com>
2019-10-22 23:40:25 +0300

    Add a User class in models.py

--- app/models.py
+++ app/models.py
@@ -3,6 +3,6 @@ from app import db
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(), unique=True)
-    email = db.Column(db.String(), unique=True)
-    password_hash = db.Column(db.String())
+    username = db.Column(db.String(), nullable=False)
+    email = db.Column(db.String(), unique=True, nullable=False)
+    password_hash = db.Column(db.String(), nullable=False)
